ac51bcf845bf49b5d71b92fc40d594077a0eeb9f
Luciano Copa <45108640@ifts24.edu.ar>
2024-12-28 20:20:28 -0300

    Agregue endpoint apiProductos

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (!grid) {
   console.log("No se encontro contenedor");
 }
 else{
-  fetch('http://localhost:8080/productosApi/getAllProductos')
+  fetch('https://apiprueba-production-4527.up.railway.app/productosApi/getAllProductos')
   .then(response => response.json())
   .then(data => {
 data.forEach(element => {
@@ -85,83 +85,3 @@ data.forEach(element => {
 
 }
 
-
-
- /* const crearHTML = (item) => {
-    const html = `
-              <article data-id="${item.id}">
-                  <h3>${item.titulo}</h3>
-                  <img src="${item.imagen}" width="200" alt="${item.titulo}">
-                  <p>${item.descripcion}</p>
-                  <p>$ ${item.precio}</p>
-                  <button type="button">Agregar</button>
-              </article>
-          `;
-  
-    return html;
-  };
-
-  const mostrarProductos = async () => {
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      console.log(response);
-      const array = await response.json();
-  
-      console.log(array);
-      const listaProductos = document.querySelector("#lista-productos");
-      listaProductos.innerHTML = "";
-      array.forEach((item) => {
-        const elementos = crearHTML(item);
-        //   console.log(elementos);
-        listaProductos.innerHTML += elementos;
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  mostrarProductos();
-*/
-
-/*
-fetch('https://example.com/api/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  console.log("Ok");
-
-  const crearHTML = (item) => {
-    const html = `
-              <article data-id="${item.id}">
-                  <h3>${item.categoria}</h3>
-                  <img src="${item.imagen}" width="200" alt="${item.titulo}">
-                  <p>${item.descripcion}</p>
-                  <p>$ ${item.precio}</p>
-                  <button type="button">Agregar</button>
-              </article>
-          `;
-  
-    return html;
-  };
-
-  const mostrarProductos = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/prodcutosApi/getAllProductos");
-      console.log(response);
-      const array = await response.json();
-  
-      console.log(array);
-      const listaProductos = document.querySelector("#lista-productos");
-      listaProductos.innerHTML = "";
-      array.forEach((item) => {
-        const elementos = crearHTML(item);
-        //   console.log(elementos);
-        listaProductos.innerHTML += elementos;
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  mostrarProductos();*/
-   
